91ba359f290b367bdf0abe6cab1a29d4a69a0667
Vladimir Bauer <vbauerster@gmail.com>
2016-02-10 22:01:41 +0300

    clean up

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -28,15 +28,8 @@ describe('the App module', () => {
     expect(sut.router.title).toEqual('Aurelia');
   });
 
-  it('should have a welcome route', () => {
-    expect(sut.router.routes).toContain({ route: ['', 'welcome'], name: 'welcome',  moduleId: 'welcome', nav: true, title: 'Welcome' });
+  it('should have a test route', () => {
+    expect(sut.router.routes).toContain({ route: ['', 'test'], name: 'test',  moduleId: 'test', nav: true, title: 'Test job' });
   });
 
-  it('should have a users route', () => {
-    expect(sut.router.routes).toContain({ route: 'users', name: 'users', moduleId: 'users', nav: true, title: 'Github Users' });
-  });
-
-  it('should have a child router route', () => {
-    expect(sut.router.routes).toContain({ route: 'child-router', name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' });
-  });
 });
